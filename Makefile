2b2ce08eedd7157ae24fb50b73c114e7c0c8026a
Bryan Ischo <bryan@ischo.com>
2008-07-29 11:45:04 +0000

    * Added TODO and COPYING * Updated copyrights to GPLv3

--- Makefile
+++ Makefile
@@ -1,3 +1,22 @@
+# Makefile
+# 
+# Copyright 2008 Bryan Ischo <bryan@ischo.com>
+# 
+# This file is part of libs3.
+# 
+# libs3 is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3 of the License.
+#
+# libs3 is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License version 3
+# along with libs3, in a file named COPYING.  If not, see
+# <http://www.gnu.org/licenses/>.
+
 
 LIBS3_VER_MAJOR := 0
 LIBS3_VER_MINOR := 2
--- src/response_headers_handler.c
+++ src/response_headers_handler.c
@@ -1,24 +1,22 @@
 /** **************************************************************************
- * response_haeders_handler.c
+ * response_headers_handler.c
  * 
  * Copyright 2008 Bryan Ischo <bryan@ischo.com>
  * 
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * This file is part of libs3.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the
+ * libs3 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3 of the License.
  *
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
+ * libs3 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with libs3, in a file named COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
  *
  ************************************************************************** **/
 
