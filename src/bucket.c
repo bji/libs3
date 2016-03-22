ad880cbbca165721f7aeb1bdf659462b6f5e904a
Alexander Sack <alexander.sack@indexengines.com>
2016-03-22 14:31:28 -0400

    Issue #36 - Expires header is set incorrectly
    The "Expires" field is not needed for a create bucket request and
    in fact, setting it to zero can cause some S3 implementations to
    fail the request because well, it has expired.
    
    Therefore, we set the "expires" S3PutParameter of the create bucket
    call to -1 in order to bypass generating this HTTP header entirely.

--- src/bucket.c
+++ src/bucket.c
@@ -263,7 +263,7 @@ void S3_create_bucket(S3Protocol protocol, const char *accessKeyId,
         0,                                       // cacheControl
         0,                                       // contentDispositionFilename
         0,                                       // contentEncoding
-        0,                                       // expires
+       -1,                                       // expires
         cannedAcl,                               // cannedAcl
         0,                                       // metaDataCount
         0,                                       // metaData
